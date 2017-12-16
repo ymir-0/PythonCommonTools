d108b11e998322988eef854777e90ca21eac94c6
ymir <mazieres.pierre@gmail.com>
2017-12-16 04:17:09 +0100

    add recursively all subpackages

--- setup.py
+++ setup.py
@@ -1,45 +1,37 @@
 #!/usr/bin/env python3
-import os
-
-import setuptools
-
-module_path = os.path.join(os.path.dirname(__file__), 'pythoncommontools/version.py')
-version_line = [line for line in open(module_path)
-                if line.startswith('__version__')][0]
-
-__version__ = version_line.split('__version__ = ')[-1][1:][:-2]
-
-import importlib
-import pkgutil
-import pythoncommontools
-from os.path import isdir
-
+# import generic modules
+from os.path import join, dirname, isdir
+from setuptools import setup
+from importlib import import_module
+from pkgutil import walk_packages
+# parse recursively a module
 # this code is an adaptation from https://stackoverflow.com/questions/3365740/how-to-import-all-submodules
-def import_submodules(package, recursive=True):
+def import_submodules(package):
     if isinstance(package, str):
-        package = importlib.import_module(package)
-    results = list()
-    for loader, name, is_pkg in pkgutil.walk_packages(package.__path__):
+        package = import_module(package)
+    modules = list()
+    for loader, name, is_pkg in walk_packages(package.__path__):
         full_name = package.__name__ + '.' + name
-        # continue recurtion only if module is folder
-        print("loader.path="+loader.path)
-        full_path=loader.path+"/"+name
-        print("full_path="+full_path)
-        if isdir(full_path):
-            results.append(full_name)
-            if recursive and is_pkg:
-                results=results+(import_submodules(full_name))
-    return results
-
-package=pythoncommontools
-results=[package.__name__]+import_submodules (package)
-print("results="+str(results))
-
-setuptools.setup(
+        full_path=join(loader.path,name)
+        # continue recursion only if module is folder
+        if is_pkg and isdir(full_path):
+            modules.append(full_name)
+            modules=modules+(import_submodules(full_name))
+    return modules
+# import and parse dedicated module
+import pythoncommontools
+rootPackage=pythoncommontools
+module_path = join(dirname(__file__), rootPackage.__name__ , "version.py")
+version_line = [line for line in open(module_path) if line.startswith("__version__")][0]
+__version__ = version_line.split('__version__ = ')[-1][1:][:-2]
+modules=[rootPackage.__name__]+import_submodules (rootPackage)
+print("loaded modules : " + str(modules))
+# define setup parameters
+setup(
     name="PythonCommonTools",
     version=__version__,
     description="common tools for Python",
-    packages=results,
+    packages=modules,
     install_requires=["psutil"],
     classifiers=[
         'Programming Language :: Python :: 3',
